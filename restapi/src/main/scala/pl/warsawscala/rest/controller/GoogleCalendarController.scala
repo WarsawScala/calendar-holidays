8a5874e5076baa83a8f0598f4420802cb94aa854
Jonatan Borkowski <jonatanborkowski@wp.pl>
2016-06-03 11:44:43 +0200

    Adds callback for oAuth2

--- restapi/src/main/scala/pl/warsawscala/rest/controller/GoogleCalendarController.scala
+++ restapi/src/main/scala/pl/warsawscala/rest/controller/GoogleCalendarController.scala
@@ -41,21 +41,31 @@ class GoogleCalendarController @Inject() (config: Configuration) extends Control
         case None =>
           //val redirect_uri = domain + s"?start=$startTime&end=$endTime"
           val params = Map(
-            RESPONSE_TYPE   -> config.getString("restapi.oAuth2.responseType").getOrElse(""),
-            CLIENT_ID       -> config.getString("restapi.oAuth2.clientId").getOrElse(""),
-            NONCE           -> config.getString("restapi.oAuth2.nonce").getOrElse(""),
-            REDIRECT_URI    -> config.getString("restapi.oAuth2.redirectURI").getOrElse(""),
+            RESPONSE_TYPE -> config.getString("restapi.oAuth2.responseType").getOrElse(""),
+            CLIENT_ID -> config.getString("restapi.oAuth2.clientId").getOrElse(""),
+            NONCE -> config.getString("restapi.oAuth2.nonce").getOrElse(""),
+            REDIRECT_URI -> config.getString("restapi.oAuth2.redirectURI").getOrElse(""),
             //REDIRECT_URI    -> redirect_uri,
-            SCOPE           -> config.getString("restapi.oAuth2.scope").getOrElse(""),
-            STATE           -> config.getString("restapi.clientName").getOrElse(""),
-            PROMPT          -> SELECT_ACCOUNT
+            SCOPE -> config.getString("restapi.oAuth2.scope").getOrElse(""),
+            STATE -> config.getString("restapi.clientName").getOrElse(""),
+            PROMPT -> SELECT_ACCOUNT
           )
 
           val uri = config.getString("restapi.oAuth2.authURL").getOrElse("") + "?" + Helpers.encodeParam(params)
           val state = counter.incrementAndGet();
-          map = map + (state -> (startTime,endTime))
+          map = map + (state ->(startTime, endTime))
           Redirect(uri)
       }
   }
 
+  def callback() = Action { request =>
+    request.queryString.get(CODE).getOrElse("") match {
+      case Some(seq) => {
+        Redirect(routes.GoogleCalendarController.holidays()).withSession {
+          CODE -> ???
+        }
+      }
+      case None => Ok("Cant't get authCode")
+    }
+  }
 }
