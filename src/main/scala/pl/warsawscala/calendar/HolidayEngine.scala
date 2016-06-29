bc494e48ce70ef443ffdb99d77d7e877272812fd
Waldemar Wosiński <kastoestoramadus@gmail.com>
2016-06-29 19:22:51 +0200

    niedokładny silnik

--- src/main/scala/pl/warsawscala/calendar/HolidayEngine.scala
+++ src/main/scala/pl/warsawscala/calendar/HolidayEngine.scala
@@ -12,18 +12,40 @@ trait HolidayEngine {
     * @param to opcjonalna data do (jęzeli brak liczymy do końca bieżącego roku)
     * @return liczbę zaplanowanych dni urlopowych
     */
-  def countHolidays(from: Option[LocalDate] = None, to: Option[LocalDate] = None): Future[Int]
+  def countHolidays(from: LocalDate, to: LocalDate): Future[Int]
 
   /**
     *
     * @param year rok dla którego liczymy pozostałe dni wolne (jeżeli brak liczymy dla bieżącego roku)
     * @return liczba pozostałych dni urlopowych
     */
-  def countHolidaysLeftInYear(year: Option[Int] = None): Future[Int]
+  def countHolidaysLeftInYear(year: Int ): Future[Int]
+}
+
+object HolidayEngineImpl {
+  val HOLIDAY_TAG = "URLAUB"
 }
 
 class HolidayEngineImpl(myCalendar: MyCalendar) extends HolidayEngine {
-  override def countHolidays(from: Option[LocalDate] = None, to: Option[LocalDate] = None): Future[Int] =
-    Future(0)
-  override def countHolidaysLeftInYear(year: Option[Int] = None): Future[Int] = ???
+  import HolidayEngineImpl._
+  /**
+    *
+    * @param from opcjonalna data od (jeżeli brak liczymy od początku bieżącego roku)
+    * @param to   opcjonalna data do (jęzeli brak liczymy do końca bieżącego roku)
+    * @return liczbę zaplanowanych dni urlopowych
+    */
+  override def countHolidays(from: LocalDate, to: LocalDate): Future[Int] = myCalendar.getEventsFor(from, to).map{ seg =>
+    seg.count( event => event.tags.contains(HOLIDAY_TAG)) // FIXME
+  }
+
+  /**
+    *
+    * @param year rok dla którego liczymy pozostałe dni wolne (jeżeli brak liczymy dla bieżącego roku)
+    * @return liczba pozostałych dni urlopowych
+    */
+  override def countHolidaysLeftInYear(year: Int): Future[Int] = {
+    val from = new LocalDate(year, 1, 1)
+    val to = new LocalDate(year, 12, 31)
+    countHolidays(from, to)
+  }
 }
